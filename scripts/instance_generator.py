ef3a701b97d0986581312543c309736ae99d9750
Vladimir Mijic <vladocodes@gmail.com>
2023-01-09 16:39:01 +0100

    instance_generator update

--- scripts/instance_generator.py
+++ scripts/instance_generator.py
@@ -1,45 +1,32 @@
 import random
 
 """
-Skripta za generisanje instanci, instance se cuvaju u txt formatu
-Cvorovi se upisuju u formatu: vertex_id vertex_weight
-Grane se upisuju u formatu: source_vertex target_vertex
+Skripta za generisanje instanci manjih dimenzija, instance se cuvaju u txt formatu.
+Generisemo grane i u txt fajl ih zapisujemo u formatu: source_vertex target_vertex
 """
 
 
-def generate_random_weighted_graph(num_vertices, num_instances):
-    if num_vertices <= 100:
-        dim = "0"
-    elif 100 < num_vertices < 400:
-        dim = "1"
-    else:
-        dim = "2"
+def generate_graph_edges(num_vertices, num_instances):
+    for p in range(len(num_vertices)):
+        n_v = num_vertices[p]
+        n_i = num_instances[p]
 
-    for k in range(num_instances):
-        # Generisi listu cvorova sa random tezinom
-        vertices = [(i, random.randint(10, 100)) for i in range(num_vertices)]
+        for k in range(0, n_i):
+            # Lista za cuvanje grana
+            edges = []
 
-        # Lista za cuvanje grana
-        edges = []
+            # Generisanje random grana izmedju cvorova
+            for i in range(1, n_v+1):
+                for j in range(1, n_v+1):
+                    if i != j and random.random() < 0.5:
+                        edges.append((i, j))
 
-        # Generisanje random grana izmedju cvorova
-        # TODO: smanjiti vjerovatnocu za dodavanje grane?
-        for i in range(num_vertices):
-            for j in range(num_vertices):
-                if i != j and random.random() < 0.5:
-                    edges.append((i, j))
-
-        # Upis informacija u fajl i cuvanje u odgovarajucem folderu u zavisnosti od dimenzije grafa
-        with open('./instances/level' + dim + '/instance' + str(k) + '.txt', 'w') as f:
-            for v in vertices:
-                f.write(f"{v[0]} {v[1]}\n")
-
-            for e in edges:
-                f.write(f"{e[0]} {e[1]}\n")
+            # Upis informacija u fajl i cuvanje u odgovarajucem folderu u zavisnosti od dimenzije grafa
+            with open('./instances/level0/instance_' + str(n_v) + "_" + str(len(edges)) + '.txt', 'w') as f:
+                for e in edges:
+                    f.write(f"{e[0]} {e[1]}\n")
 
 
 if __name__ == '__main__':
-    generate_random_weighted_graph(5, 10)
-    generate_random_weighted_graph(300, 10)
-    generate_random_weighted_graph(500, 10)
-    print("Generisanje je zavrseno.")
+    generate_graph_edges([5, 10, 15, 50], [3, 3, 2, 2])
+    print("Completed")
