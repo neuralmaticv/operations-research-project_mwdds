d96adc8323603396afc5616387a94fc959f9e518
Vladimir Mijic <vladocodes@gmail.com>
2023-09-11 03:50:49 +0200

    ga update

--- main/genetic_algorithm.py
+++ main/genetic_algorithm.py
@@ -3,11 +3,44 @@ import time
 import logging
 
 logger = logging.getLogger("main")
-def fitness_func(graph, individual):
-    # Convert the binary string to a set of selected vertices
-    selected_vertices = set(i for i, bit in enumerate(individual.chromosome) if bit == 1)
+
+def is_valid(graph, individual, initial=False):
+    graph.reset_colors()
+
+    logger.info(f"Individual: {individual}")
+
+    for i in range(len(individual)):
+        if individual[i] == 1:
+            graph.color_vertex(graph.vertices[i], 2)
     
-    return sum(graph.vertices[v].weight for v in selected_vertices), len(selected_vertices)
+    logger.info(graph.white_vertices)
+    logger.info(graph.gray_vertices)
+    logger.info(graph.black_vertices)
+    logger.info(sum([vertex.weight for vertex in graph.black_vertices]))
+    logger.info("")
+    logger.info("")
+            
+    if len(graph.white_vertices) == 0:
+        return True
+    
+    if initial:
+        if len(graph.white_vertices) > 0:
+            for i in range(len(individual)):
+                if random.random() < 0.8 and individual[i] == 0 and graph.vertices[i].color == 0:
+                    individual[i] = 1
+                    graph.color_vertex(graph.vertices[i], 2)
+                    
+            if len(graph.white_vertices) == 0:
+                return True
+    
+    return False
+
+def fitness_func(graph, individual, initial=False):
+    if is_valid(graph, individual.chromosome, initial):
+        return sum([graph.vertices[i].weight for i, _ in enumerate(individual.chromosome) if individual.chromosome[i] == 1])
+    else:
+        return graph.total_weight
+
 
 class Individual:
     def __init__(self, init=None, chromosome_length=0):
@@ -20,26 +53,23 @@ class Individual:
             self.chromosome_length = len(init)
             self.fitness = 0
 
-    def mutate(self, graph):
-        if random.uniform(0, 1) > 0.5:
-            # swap
-            logger.info("Swapping")
-            logger.info(f"Before: {self.chromosome}")
-            idx1, idx2 = self.get_uniqe_ids()
+    def mutate(self):
+        # swap
+        # logger.info("Swapping")
+        # logger.info(f"Before: {self.chromosome}")
+        # idx1, idx2 = self.get_uniqe_ids()
+        
+        # self.chromosome[idx1], self.chromosome[idx2] = self.chromosome[idx2], self.chromosome[idx1]
+        # logger.info(f"After: {self.chromosome}")
             
-            self.chromosome[idx1], self.chromosome[idx2] = self.chromosome[idx2], self.chromosome[idx1]
-            logger.info(f"After: {self.chromosome}")
-        else:
-            logger.info("Rotating")
-            logger.info(f"Before: {self.chromosome}")
-            idx1, idx2 = self.get_uniqe_ids()
-            
-            fst = idx1 if graph.vertices[idx1].weight < graph.vertices[idx2].weight else idx2
-            snd = idx2 if graph.vertices[idx1].weight < graph.vertices[idx2].weight else idx1
-
-            self.chromosome = self.chromosome[:fst] + self.chromosome[fst:snd][::-1] + self.chromosome[snd:]
-            logger.info(f"After: {self.chromosome}")
-            
+        # return self
+        logger.info("Mutating")
+        logger.info(f"Before: {self.chromosome}")
+        for i in range(len(self.chromosome)):
+            if random.random() < 0.5:
+                self.chromosome[i] = 1 - self.chromosome[i]
+        logger.info(f"After: {self.chromosome}")
+                
         return self
     
     def get_uniqe_ids(self):
@@ -64,29 +94,80 @@ class Population:
         self.normalized_proportions = None
         self.graph = graph
         self.source_vertices = [vertex.id - 1 for vertex in graph.source_vertices] # vertices with in-degree 0
-        self.individuals = [self._create_individual(graph) for _ in range(population_size - 1)]
+        self.individuals = self._initialize_population()
+
+    def _gen_by_source_vertices(self, max_number):
+        chromosome_length = len(self.graph.vertices)
+        population = []
+
+        for _ in range(max_number):
+            individual = [0] * chromosome_length
+
+            for i in range(chromosome_length):
+                if i in self.source_vertices:
+                    individual[i] = 1
+                else:
+                    individual[i] = random.randint(0, 1)
+            
+            individual = Individual(init=individual)
+            population.append(individual)
         
+        return population
+    
+    def _gen_random(self, max_number):
+        chromosome_length = len(self.graph.vertices)
+        population = []
 
-    def _create_individual(self, graph):
-        chromosome_length = len(graph.vertices)
+        for _ in range(max_number):
+            individual = [0] * chromosome_length
 
-        # Initialize an individual with all bits set to 0
-        individual = [0] * chromosome_length
-
-        # Ensure that all vertices with in-degree 0 are selected
-        for i in range(chromosome_length):
-            if i in self.source_vertices:
-                individual[i] = 1
-            else:
-                # Set the rest of the bits randomly
-                individual[i] = random.randint(0, 1)
-
+            for i in range(chromosome_length):
+                if i not in self.source_vertices:
+                    individual[i] = random.randint(0, 1)
+            
+            individual = Individual(init=individual)
+            population.append(individual)
         
-        # Create an Individual object if needed
-        initial = Individual(individual)
-        initial.fitness, _ = fitness_func(graph, initial)
+        return population
+    
+    def _gen_by_highest_degree_vertices(self, max_number):
+        chromosome_length = len(self.graph.vertices)
+        population = []
 
-        return initial
+        for _ in range(max_number):
+            individual = [0] * chromosome_length
+
+            for i in range(chromosome_length):
+                if self.graph.vertices[i].in_degree > 2 and self.graph.vertices[i].out_degree > 2:
+                    individual[i] = 1
+                elif i not in self.source_vertices:
+                    individual[i] = random.randint(0, 1)
+            
+            individual = Individual(init=individual)
+            population.append(individual)
+        
+        return population
+
+    def _initialize_population(self):
+        chromosome_length = len(self.graph.vertices)
+        population = []
+
+        # generate individuals with source vertices
+        max_source_individuals = int(self.population_size * 0.3)
+        population += self._gen_by_source_vertices(max_source_individuals)
+
+        # generate random individuals
+        max_random_individuals = self.population_size - max_source_individuals
+        population += self._gen_random(max_random_individuals)
+
+        # generate individuals with highest degree vertices
+        # max_highest_degree_individuals = self.population_size - max_source_individuals - max_random_individuals
+        # population += self._gen_by_highest_degree_vertices(max_highest_degree_individuals)
+
+        for individual in population:
+            individual.fitness = fitness_func(self.graph, individual, initial=True)
+    
+        return population
 
     def _calc_normalized_proportions(self):
         all_fitnesses = [individual.fitness for individual in self.individuals]
@@ -101,11 +182,7 @@ class Population:
         
         self.normalized_proportions = normalized_proportions
 
-    def calculate_fitness(self, vertices_w, edges):
-        for individual in self.individuals:
-            individual.fitness_func(vertices_w, edges)
-
-    def selection(self, method='roulette_wheel'):
+    def selection(self, method='tournament'):
         if method == 'roulette_wheel':
             total = 0.0
             best_individual = min(self.individuals, key=lambda individual: individual.fitness)
@@ -122,7 +199,18 @@ class Population:
             
             # If we didn't find an individual, return the best one
             return best_individual
-        
+        elif method == 'tournament':
+            # Select two random individuals
+            individual1 = random.choice(self.individuals)
+            individual2 = random.choice(self.individuals)
+            
+            # Ensure that the two selected individuals are different
+            while individual1 == individual2:
+                individual2 = random.choice(self.individuals)
+            
+            # Select the better of the two individuals
+            return min(individual1, individual2, key=lambda individual: individual.fitness)
+
     def single_point_crossover(self, individual1, individual2, mutation_probability=0.05):
         crossover_point = random.randint(1, len(individual1.chromosome) - 1)
 
@@ -141,8 +229,8 @@ class Population:
                 child1.chromosome[i] = 1
                 child2.chromosome[i] = 1
             
-        child1.fitness,_ = fitness_func(self.graph, child1)
-        child2.fitness,_ = fitness_func(self.graph, child2)        
+        child1.fitness = fitness_func(self.graph, child1)
+        child2.fitness= fitness_func(self.graph, child2)        
         
         return child1, child2
     
@@ -151,18 +239,25 @@ class Population:
         new_child2 = Individual(init=child2.chromosome)
 
         if random.uniform(0, 1) < mutation_probability:
-            new_child1 = child1.mutate(self.graph)
+            new_child1 = child1.mutate()
         
         if random.uniform(0, 1) < mutation_probability:
-            new_child2 = child2.mutate(self.graph)
+            new_child2 = child2.mutate()
         
         return new_child1, new_child2
-    
+
+
 class GeneticAlgorithm:
-    def __init__(self, graph, fitness_fn, max_time=None, **kwargs):
+    def __init__(self, graph, fitness_fn, max_time=None, max_no_improvement=None, **kwargs):
         self.graph = graph
         self.fitness_fn = fitness_fn
         self.max_time = max_time
+        self.fitness_over_time = []
+        self.max_no_improvement = max_no_improvement
+        self.gen_counter = 0
+        self.no_improvement_counter = 0
+        self.previous_best_fitness = graph.total_weight
+        self.running_time = 0
         
         # Update class attributes with values from kwargs
         for key, value in kwargs.items():
@@ -173,54 +268,86 @@ class GeneticAlgorithm:
         if not hasattr(self, 'population_size'):
             self.population_size = 100
         if not hasattr(self, 'selection_method'):
-            self.selection_method = 'roulette_wheel'
+            self.selection_method = 'tournament'
         if not hasattr(self, 'crossover_probability'):
             self.crossover_probability = 0.8
         if not hasattr(self, 'mutation_probability'):
             self.mutation_probability = 0.05
+        if not hasattr(self, 'mutation_increase_factor'):
+            self.mutation_increase_factor = 1.1
+        if not hasattr(self, 'inc_mutation'):
+            self.inc_mutation = 10
         
         self.population = Population(self.graph, self.population_size)
+    
+    def get_fitness_over_time(self):
+        return self.fitness_over_time
 
+    def has_converged(self):
+        if self.gen_counter >= self.n_generations or self.no_improvement_counter >= self.max_no_improvement:
+            return True
+        return False
+
+    def get_running_time(self):
+        return self.running_time
 
     def run(self):
         logger.info("Initial population:")
         for individual in self.population.individuals:
             logger.info(f"chromosome: {individual.chromosome}, fitness: {individual.fitness}")
         self.population._calc_normalized_proportions()
-
+        
+        
         logger.info("Starting the algorithm...")
         start = time.time()
-        while self.n_generations > 0:
-            logger.info(f"Generation {self.n_generations}")
+        while (not self.has_converged()) and (self.running_time < self.max_time):
+            logger.info(f"Generation {self.gen_counter}")
 
             logger.info("Selection...")
-            individual1 = self.population.selection(self.selection_method)
-            individual2 = self.population.selection(self.selection_method)
+            # select 1/2 of the initial population
+            selected_individuals = []
+            for _ in range(int(self.population_size / 2)):
+                selected_individuals.append(self.population.selection(self.selection_method))
             
-            # make sure that we dont select the same individuals
-            while (individual1 == individual2):
-                individual2 = self.population.selection(self.selection_method)
+            for i in range(0, len(selected_individuals), 2):
+                individual1 = selected_individuals[i]
+                individual2 = selected_individuals[i + 1]
 
-            logger.info(f"individual1: {individual1.chromosome}, fitness: {individual1.fitness}")
-            logger.info(f"individual2: {individual2.chromosome}, fitness: {individual2.fitness}")
-            
-            logger.info("Crossover...")
-            ch1, ch2 = self.population.single_point_crossover(individual1, individual2)
-            logger.info(f"ch1: {ch1.chromosome}, fitness: {ch1.fitness}")
-            logger.info(f"ch2: {ch2.chromosome}, fitness: {ch2.fitness}")
+                # crossover of selected individuals
+                if random.uniform(0, 1) < self.crossover_probability:
+                    logger.info("Crossover...")
+                    ch1, ch2 = self.population.single_point_crossover(individual1, individual2, self.mutation_probability)
+                    self.population.individuals.append(ch1)
+                    self.population.individuals.append(ch2)                 
 
-            # add children to the population and remove the worst individuals
-            self.population.individuals.append(ch1)
-            self.population.individuals.append(ch2)
+            logger.info(f"population size: {len(self.population.individuals)}")
             self.population.individuals = sorted(self.population.individuals, key=lambda individual: individual.fitness)[:self.population_size]
-            logger.info(f"Population size: {len(self.population.individuals)}")
-            for individual in self.population.individuals:
-                logger.info(f"chromosome: {individual.chromosome}, fitness: {individual.fitness}")
-
-            self.n_generations -= 1
+            logger.info(f"population size: {len(self.population.individuals)}")
             
-            if time.time() - start > self.max_time:
-                logger.info("Time limit reached.")
-                break
+            best_fitness = self.population.individuals[0].fitness
+            self.fitness_over_time.append(best_fitness)
+            logger.info(f"Best fitness: {best_fitness}")
+
+            if self.previous_best_fitness == best_fitness:
+                self.no_improvement_counter += 1
+                if self.no_improvement_counter >= self.inc_mutation:
+                    self.mutation_probability *= self.mutation_increase_factor
+                    self.mutation_probability = min(self.mutation_probability, 0.5) # max mutation probability is 0.5
+                    logger.info(f"Mutation probability increased to {self.mutation_probability}")
+            else:
+                self.no_improvement_counter = 0
+                self.previous_best_fitness = best_fitness
+
+            self.gen_counter += 1
+            self.running_time = time.time() - start
+
+        logger.info(f"Best solution: {self.population.individuals[0].chromosome}")
+        logger.info(f"Best fitness: {self.population.individuals[0].fitness}")
+        if self.gen_counter == self.n_generations:
+            logger.info("Algorithm finished due to reaching the maximum number of generations.")
+        elif self.no_improvement_counter == self.max_no_improvement:
+            logger.info("Algorithm finished due to reaching the maximum number of generations without improvement.")
+        else:
+            logger.info("Algorithm finished due to reaching the maximum time.")
         
         logger.info("Algorithm finished.")
--- main/graph.py
+++ main/graph.py
@@ -1,11 +1,15 @@
+import logging
+logger = logging.getLogger("main")
+
 class Vertex:
-    def __init__(self, id, weight):
+    def __init__(self, id, weight, color=0):
         self.id = id
         self.weight = weight
         self.successors = []
         self.predecessors = []
         self.in_degree = 0
         self.out_degree = 0
+        self.color = color # 0 - white, 1 - gray, 2 - black
 
     def add_successor(self, successor):
         self.successors.append(successor)
@@ -16,7 +20,7 @@ class Vertex:
         self.in_degree += 1
 
     def __repr__(self) -> str:
-        return f"Vertex(id={self.id}, weight={self.weight}, in_degree={self.in_degree}, out_degree={self.out_degree})"
+        return f"V(id={self.id}, w={self.weight}, in_d={self.in_degree}, out_d={self.out_degree}, color={self.color})"
     
     def __str__(self) -> str:
         return self.__repr__()
@@ -27,10 +31,17 @@ class Graph:
         self.vertices = []
         self.edges = []
         self.source_vertices = []
+        self.white_vertices = []
+        self.gray_vertices = []
+        self.black_vertices = []
+        self.total_weight = 0
 
     def create_from_data(self, vertices_weights, edges):
         for vertex_id, weight in vertices_weights.items():
-            self.vertices.append(Vertex(vertex_id, weight))
+            v = Vertex(vertex_id, weight)
+            self.vertices.append(v)
+            self.white_vertices.append(v)
+            self.total_weight += weight
 
         for edge in edges:
             source, target = edge
@@ -42,19 +53,66 @@ class Graph:
                 target_vertex.add_predecessor(source_vertex)
                 self.edges.append(edge)
 
-        for vertex in self.vertices:
+        for vertex in self.vertices:            
             if vertex.in_degree == 0:
                 self.source_vertices.append(vertex)
+                self.white_vertices.remove(vertex)
+                vertex.color = 2 # source vertices are included - black
+                self.black_vertices.append(vertex)
+                for successor in vertex.successors:
+                    if successor.color == 0:
+                        self.white_vertices.remove(successor)
+                        successor.color = 1
+                        self.gray_vertices.append(successor)
+                    
+    def color_vertex(self, vertex, color):
+        if vertex.color == 0:
+            if color == 1:
+                self.white_vertices.remove(vertex)
+                vertex.color = color
+                self.gray_vertices.append(vertex)
+                # logger.info(f"vertex {vertex.id}: w -> g")
+            elif color == 2:
+                self.white_vertices.remove(vertex)
+                vertex.color = color
+                self.black_vertices.append(vertex)
+                for successor in vertex.successors:
+                    if successor.color == 0:
+                        self.color_vertex(successor, 1)
+                # logger.info(f"vertex {vertex.id}: w -> b")
+        elif vertex.color == 1:
+            if color == 2:
+                self.gray_vertices.remove(vertex)
+                vertex.color = color
+                self.black_vertices.append(vertex)
+                for successor in vertex.successors:
+                    if successor.color == 0:
+                        self.color_vertex(successor, 1)
+                # logger.info(f"vertex {vertex.id}: g -> b")
+        
+        if len(self.vertices) != (len(self.black_vertices) + len(self.gray_vertices) + len(self.white_vertices)):
+            logger.error("Number of vertices is not equal to the sum of colored vertices")
+            raise Exception("Number of vertices is not equal to the sum of colored vertices")
 
+                
     def get_vertex_by_id(self, vertex_id):
         for vertex in self.vertices:
             if vertex.id == vertex_id:
                 return vertex
         return None
     
+    def reset_colors(self):
+        self.white_vertices = self.vertices.copy()
+        self.gray_vertices = []
+        self.black_vertices = []
+        for vertex in self.vertices:
+            vertex.color = 0
+        
+        # for vertex in self.source_vertices:
+        #     self.color_vertex(vertex, 2)
+    
     def __repr__(self) -> str:
         return f"Graph(vertices={self.vertices}, edges={self.edges})"
     
     def __str__(self) -> str:
         return self.__repr__()
-    
